2257cb7cfd2e4a3f8998fd41a7b3fde846fc00d4
ihruan <111167846+ihruan@users.noreply.github.com>
2022-08-14 18:06:40 +0200

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,15 @@
+﻿import random
+
+print("V's Password Generator 1.0")
+
+characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()"
+
+passwordLength = int(input("Input Password Length Here " ""))
+
+newPassword = []
+for x in range(passwordLength):
+  newPassword.append(random.choice(characters))
+
+  finalPassword = ''.join(map(str, newPassword))
+
+  print("\n This is your new password: ", finalPassword)
